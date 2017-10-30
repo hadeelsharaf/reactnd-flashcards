7afb479a2a77f0f5fbfae9c3756dc04bcc93c67f
hadeelsharaf <hadeel.sharaf.aa@gmail.com>
2017-10-30 23:46:22 +0200

    fix :  add status bar and the hight

--- App.js
+++ App.js
@@ -1,23 +1,22 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View,Text,StatusBar } from 'react-native';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './reducers'
+import Decklist from './components/deckList';
+import { Constants } from 'expo';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
+      <Provider store={createStore(reducer)}>
+        <View style={{flex: 1}}>
+           <View style={{ height: Constants.statusBarHeight }}>
+              <StatusBar translucent barStyle='dark-content' />
+          </View>
+          <Decklist />
+        </View>
+      </Provider>
+    )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
