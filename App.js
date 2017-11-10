b6ac1600b9a320b2416b754d6398c35ee6961693
hadeel <hadeel.sharaf.aa@gmail.com>
2017-11-10 23:34:19 +0200

    feat:tabs and stack navigation

--- App.js
+++ App.js
@@ -3,16 +3,28 @@ import { View,Text,StatusBar } from 'react-native';
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
+import DeckDetail from './components/details';
 import Decklist from './components/deckList';
 import AddDeck from './components/addDeck';
+import AddCard from './components/addCard';
 import { Constants } from 'expo';
 import {
-  StackNavigator,
+  TabNavigator,
+  StackNavigator
 } from 'react-navigation';
 
-const MainNav = StackNavigator({
+const Tabs = TabNavigator({
   Home: { screen: Decklist },
   AddDeck: { screen: AddDeck },
+}, {
+  navigationOptions: {
+    header: null
+}});
+
+const MainNav = StackNavigator({
+  Tabs:  {screen: Tabs },
+  Details : {screen: DeckDetail },
+  AddCard : {screen: AddCard },
 });
 
 
